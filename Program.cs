784905230ddc8bb40c121499fdd197b086b20251
lexvonghost <lex_von_tost@mail.ru>
2022-07-24 20:51:43 +0300

    feat: решение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random rand = new Random();
+
+int n = rand.Next(100, 1000);
+int digit1 = n / 100;
+int digit3 = n % 10;
+Console.WriteLine(n);
+Console.WriteLine(digit1 * 10 + digit3);// Решение 1 (математическое)
+Console.WriteLine("" + digit1 + digit3);// Решение 2 (склеивание строк)
+Console.WriteLine($"{digit1}{digit3}");//  Решение 3
